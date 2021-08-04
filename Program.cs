0d03a09a796aa346a90c839b5dfbfed699606ea2
mholsbrink <mh.holsbrink@gmail.com>
2021-08-04 21:26:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            var range = Enumerable.Range(1, 100);            
+
+            foreach (var i in range)
+            {
+                var output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (output.Length <= 0)
+                {
+                    output += i;
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
